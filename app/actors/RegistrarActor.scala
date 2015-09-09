092ade6500c11727751cf09ba7928a7bbfe3dae4
Brandon Parmenter <leftiness@gmail.com>
2015-09-08 22:37:06 -0400

    Added CRUD operations
    Still have to rewrite a lot of stuff to use those...

--- app/actors/RegistrarActor.scala
+++ app/actors/RegistrarActor.scala
@@ -1,11 +1,42 @@
 package actors
 
 import akka.actor._
+import anorm._
+import anorm.SqlParser._
+import java.util.Date
+import play.api.db._
 import play.api.Logger
 import play.api.Play.current
 import scala.collection.mutable._
+import scala.math.BigInt
 
 import messages._
+
+case class Room(id: BigInt, name: String)
+case class User(id: BigInt, room: BigInt, name: String, joined: Date, admin: Boolean, banned: Boolean)
+
+object Room {
+    val parser: RowParser[Room] = {
+        get[BigInt]("rooms.id") ~
+        str("rooms.name") map {
+            case id ~ name => Room(id, name)
+        }
+    }
+}
+
+object User {
+    val parser: RowParser[User] = {
+        get[BigInt]("users.id") ~
+        get[BigInt]("users.room") ~
+        str("users.name") ~
+        date("users.joined") ~
+        bool("users.admin") ~
+        bool("users.banned") map {
+            case user ~ room ~ name ~ joined ~ admin ~ banned =>
+                User(user, room, name, joined, admin, banned)
+        }
+    }
+}
 
 class RegistrarActor extends Actor {
     val registry = new HashMap[String, Map[String, ActorRef]]()
@@ -35,19 +66,117 @@ class RegistrarActor extends Actor {
         return s"The name $name is already taken"
     }
     
-    def getUniqueId(): String = {
-        // TODO This is not a proper unique id...
-        return (System.currentTimeMillis / 1000).toString()
+    def newRoom(name: String): Option[BigInt] = {
+        Logger.debug(s"Creating new room: $name")
+        DB.withConnection { implicit c =>
+            return SQL"insert into rooms (name) values (name)"
+                .executeInsert(get[BigInt]("id").singleOpt)
+        }
     }
     
-    // TODO There's quite a bit of repetitive code here...
+    def getRoom(id: BigInt): Option[Room] = {
+        Logger.debug(s"Retrieving room: $id")
+        DB.withConnection { implicit c =>
+            return SQL"select (id, name) from rooms where id = '$id'"
+                .as(Room.parser.singleOpt)
+        }
+    }
+    
+    def nameRoom(id: BigInt, name: String): Integer = {
+        Logger.debug(s"Renaming room: $id, $name")
+        DB.withConnection { implicit c =>
+            return SQL"update rooms set name = '$name' where id = '$id'"
+                .executeUpdate()
+        }
+    }
+    
+    def deleteRoom(id: BigInt): Integer = {
+        Logger.debug(s"Deleting room: $id")
+        DB.withConnection { implicit c =>
+            return SQL"delete from rooms where id = '$id'"
+                    .executeUpdate()
+        }
+    }
+    
+    def joinRoom(id: BigInt): Option[BigInt] = {
+        Logger.debug(s"Joining room: $id")
+        DB.withConnection { implicit c =>
+            return SQL"insert into users (room) values ('$id')"
+                .executeInsert(get[BigInt]("id").singleOpt)
+        }
+    }
+    
+    def leaveRoom(user: BigInt, room: BigInt): Integer = {
+        Logger.debug(s"Leaving room: $user, $room")
+        DB.withConnection { implicit c =>
+            return SQL"delete from users where id = '$user' and room = '$room'"
+                .executeUpdate()
+        }
+    }
+    
+    def getUser(id: BigInt): Option[User] = {
+        Logger.debug(s"Getting user: $id")
+        DB.withConnection { implicit c =>
+            return SQL"""select (id, room, name, joined, admin, banned) 
+                from users
+                where id = '$id'
+                """
+                .as(User.parser.singleOpt)
+        }
+    }
+    
+    def getUsers(room: BigInt): List[User] = {
+        Logger.debug(s"Getting users in room: $room")
+        DB.withConnection { implicit c =>
+            return SQL"""select (id, room, name, joined, admin, banned)
+                from users
+                where room = '$room'
+                """
+                .as(User.parser.*)
+        }
+    }
+    
+    def nameUser(id: BigInt, name: String): Integer = {
+        Logger.debug(s"Renaming user: $id, $name")
+        DB.withConnection { implicit c =>
+            return SQL"update users set name = '$name' where id = '$id'"
+                .executeUpdate()
+        }
+    }
+    
+    def promoteUser(id: BigInt): Integer = {
+        Logger.debug(s"Promoting user: $id")
+        DB.withConnection { implicit c =>
+            return SQL"update users set admin = true where id = '$id'"
+                .executeUpdate()
+        }
+    }
+    
+    def banUser(id: BigInt): Integer = {
+        Logger.debug(s"Banning user: $id")
+        DB.withConnection { implicit c =>
+            return SQL"update users set banned = true where id = '$id'"
+                .executeUpdate()
+        }
+    }
+    
+    // TODO 
+    // There's quite a bit of repetitive code down there...
+    // The entire receive method will be rewritten using the database stuff above.
+    // The messages will need to be rewritten. 
+        // For example, a join will be Join(id: BigInt). Users (front-end code) will know their bigint to join the room. It's the ID in chat.com/id.
+        // For example, a New(name: String) message doesn't even exist yet, but it will be used to create a new room when a user doesn't have an id to join.
+            // A New() message will create a room, put the user in that room, make that user the admin, and so on.
+    // When users leave a room, check if there are still admins in the room. If there aren't any, promote the user who has the oldest join date.
+    // That joinRoom method might be confusing when I see it tomorrow. Think of it this way. The user has no ID or no name until he joins a room.
+        // When he joins the room, that method returns an ID for him. That's now his ID, and it's also his name until he renames himself.
     
     def receive = {
         case Join(room: String) =>
             Logger.debug(s"Received a join: $sender.path $room")
             registry get room match {
                 case Some(users: Map[String, ActorRef]) =>
-                    val name = getUniqueId()
+                    val name = "foo" //getUniqueId() TODO This whole section will be rewritten.
                     Logger.debug(s"Giving sender the name $name")
                     sender ! Name("", room, name)
                     Logger.debug(s"Adding $name to $room")
@@ -57,7 +186,7 @@ class RegistrarActor extends Actor {
                         ref ! Message(registrar, room, userJoined(name))
                     }
                 case None =>
-                    val name = getUniqueId()
+                    val name = "foo" //getUniqueId() TODO This whole section will be rewritten.
                     Logger.debug(s"Giving sender the name $name")
                     sender ! Name("", room, name)
                     Logger.debug(s"Creating room $room")
